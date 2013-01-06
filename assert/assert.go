59d44929793bbb0eafa19f79ff488a9e34279819
ycat <liulitom@gmail.com>
2013-01-06 23:47:34 +0800

    provide equals for various types: slice, array, map and interface{} allow customize error message

--- assert/assert.go
+++ assert/assert.go
@@ -3,7 +3,11 @@ package assert
 import (
 	"testing"
 	"runtime"
+	"os"
+	"path/filepath"
+	"errors"
 	"fmt"
+	"reflect"
 )
 
 type Assert struct {
@@ -14,25 +18,162 @@ func New(t *testing.T) Assert {
 	return Assert {t}
 }
 
-func (as *Assert) reportError(errorMsg string, cd int) {
-	_, file, line, ok := runtime.Caller(cd); 
-	if !ok {
-		file = "?"
-		line = 0
-	} 
-	as.t.Errorf("%v:%v: %v", file, line, errorMsg)
-}
-
-func (as *Assert) NoError(err error) {
+func relativePath(file string) (relpath string, err error) {
+	wd, err := os.Getwd()
 	if err != nil {
-		errorMsg := fmt.Sprintf("%v", err)
-		as.reportError(errorMsg, 2)
-	}		
+		return
+	}
+	relpath, err = filepath.Rel(wd, file)
+	return
 }
 
-func (as *Assert) Equals(expected interface{}, actual interface{}) {
-	if actual != expected {
-		errorMsg := fmt.Sprintf("%v != %v", expected, actual)
-		as.reportError(errorMsg, 2)
+func (as *Assert) reportError(cd int, format string, a ...interface{}) {
+	_, file, line, ok := runtime.Caller(cd)
+	if ok {
+		file, _ = relativePath(file)
+	} else {
+		file = "?"
 	}
+	msg := fmt.Sprintf(format, a...)
+	as.t.Errorf("%v:%v: %v", file, line, msg)
+}
+
+
+type varArgsFunc func (...interface{})
+type Predicate func (...interface{}) (bool, error)
+
+func AllNil() Predicate {
+	return func(ops ...interface{}) (result bool, err error) {
+		result = true
+		for index := 0; index < len(ops); index ++ {
+			if ops[index] != nil {
+				result = false
+				return
+			}
+		}
+		return 
+	}
+}
+
+func Not(pred Predicate) Predicate {
+	return func(ops ...interface{}) (bool, error) {
+		result, err := pred(ops...)
+		return !result, err
+	}
+}
+
+
+func equalSlice(op1 interface{}, op2 interface{}) bool {
+	s1 := reflect.ValueOf(op1)
+	s2 := reflect.ValueOf(op2)
+	if s1.Len() == s2.Len() {
+		for index := 0; index < s1.Len(); index ++ {
+			if !equal(s1.Index(index).Interface(), s2.Index(index).Interface()) {
+				return false
+			}
+		}
+		return true
+	} 
+	return false
+}
+
+func equalMap(op1 interface{}, op2 interface{}) bool {
+	m1 := reflect.ValueOf(op1)
+	m2 := reflect.ValueOf(op2)
+	if m1.Len() == m2.Len() {
+		keys := m1.MapKeys()
+		for _, key := range keys {
+			value1 := m1.MapIndex(key)
+			value2 := m2.MapIndex(key)
+			if !value2.IsValid() {
+				return false
+			}
+			if !equal(value1.Interface(), value2.Interface()) {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func alwaysNotEqual(op1 interface{}, op2 interface{}) bool {
+	return false
+}
+
+func selectEqualFunction(op1 interface{}, op2 interface{}) (func (interface{}, interface{}) bool) {
+	kind1 := reflect.TypeOf(op1).Kind()
+	kind2 := reflect.TypeOf(op2).Kind()
+	
+	isSlice1 := kind1 == reflect.Slice || kind1 == reflect.Array
+	isSlice2 := kind2 == reflect.Slice || kind2 == reflect.Array
+
+	if isSlice1 != isSlice2 {
+		return alwaysNotEqual
+	}
+	
+	if isSlice1 && isSlice2 {
+		return equalSlice
+	}
+
+	isMap1 := kind1 == reflect.Map
+	isMap2 := kind2 == reflect.Map
+	if isMap1 != isMap2 {
+		return alwaysNotEqual
+	}
+	
+	if isMap1 && isMap2 {
+		return equalMap
+	}
+	
+	return equalDefault
+}
+
+func equalDefault(op1 interface{}, op2 interface{}) bool {
+	return op1 == op2
+}
+
+func equal(op1 interface{}, op2 interface{}) bool {
+	return selectEqualFunction(op1, op2)(op1, op2)
+}
+
+func Eq() Predicate {
+	return func(ops ...interface{}) (result bool, err error) {
+		if len(ops) <= 1 {
+			err = errors.New(fmt.Sprintf("too few arguments for Equivalance: %v", ops))
+			return
+		}
+		op0 := ops[0]
+		result = true
+		for index := 1; index < len(ops); index ++ {
+			if !equal(op0, ops[index]) {
+				result = false
+				return
+			}
+		}
+		return
+	}
+}
+
+func (as *Assert) Assert(pred Predicate, format string, a ...interface{}) varArgsFunc {
+	return func(ops ...interface{}) {
+		result, err := pred(ops...)
+		if err != nil {
+			as.reportError(2, "%v", err)
+		} else if !result {
+			as.reportError(2, format, a...)
+		}
+	}
+}
+
+func (as *Assert) IsNil(format string, a ...interface{}) varArgsFunc  {
+	return as.Assert(AllNil(), format, a...)
+}
+
+func (as *Assert) Equal(format string, a ...interface{}) varArgsFunc {
+	return as.Assert(Eq(), format, a...)
+}
+
+func (as *Assert) NotEqual(format string, a ...interface{}) varArgsFunc {
+	return as.Assert(Not(Eq()), format, a...)
 }
---
+++ assert/assert_test.go
@@ -0,0 +1,102 @@
+package assert
+
+import "testing"
+
+func TestEq(t* testing.T) {
+	as := New(t)
+	if result, _ := Eq()(1, 1); !result {
+		as.reportError(1, "Eq()(1, 1) expects true but got false")
+	}
+	if result, _ := Eq()(1, 1, 1); !result {
+		as.reportError(1, "Eq()(1, 1, 1) expects true but got false")
+	}
+	if result, _ := Eq()(1, 2); result {
+		as.reportError(1, "Eq()(1, 2) expects false but got true")
+	}
+	if result, _ := Eq()(1, 2, 1); result {
+		as.reportError(1, "Eq()(1, 2, 1) expects false but got true")
+	}
+	as.Equal("(%v, %v) should be equivalent", 1, 1)(1, 1)
+	as.Equal("(%v, %v, %v) should be equivalent", 1, 1, 1)(1, 1, 1)
+	as.NotEqual("(%v, %v) should not be equivalent", 1, 2)(1, 2)
+	as.NotEqual("(%v, %v, %v) should not be equivalent", 1, 2, 1)(1, 2, 1)
+
+	a1 := [3]byte{1, 2, 3}
+	a2 := [3]byte{1, 2, 3}
+	as.Equal("(%v, %v) should be equivalent", a1, a2)(a1, a2)
+
+	a3 := [3]byte{1, 3, 2}
+	a4 := [3]byte{1, 2, 3}
+	as.NotEqual("(%v, %v) should not be equivalent", a3, a4)(a3, a4)
+
+	s1 := a1[1:]
+	s2 := a2[1:]
+	as.Equal("(%v, %v) should be equivalent", s1, s2)(s1, s2)
+
+	s3 := a3[:]
+	s4 := a4[:]
+	as.NotEqual("(%v, %v) should not be equivalent", s3, s4)(s3, s4)
+
+	a5 := [3][3]byte{ {1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	a6 := [3][3]byte{ {1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	as.Equal("(%v, %v) should be equivalent", a5, a6)(a5, a6)
+
+	s5 := a5[:]
+	s6 := a6[:]
+	as.Equal("(%v, %v) should be equivalent", s5, s6)(s5, s6)
+
+	s7 := a5[0:2]
+	s8 := a6[1:3]
+	as.NotEqual("(%v, %v) should not be equivalent", s7, s8)(s7, s8)
+	
+	a7 := [2][]byte{s1, s2}
+	a8 := [2][]byte{s1, s2}
+	as.Equal("(%v, %v) should be equivalent", a7, a8)(a7, a8)
+	
+	s9 := a7[:]
+	s10 := a8[:]
+	as.Equal("(%v, %v) should be equivalent", s9, s10)(s9, s10)
+
+	m1 := make(map[string] int)
+	m2 := make(map[string] int)
+	as.Equal("(%v, %v) should be equivalent", m1, m2)(m1, m2)
+	
+	m3 := map[string] string {"k1":"v1", "k2":"v2"}
+	m4 := map[string] string {"k2":"v2", "k1":"v1"}
+	as.Equal("(%v, %v) should be equivalent", m3, m4)(m3, m4)
+
+	m5 := map[string] string {"k1":"v1", "k2":"v3"}
+	m6 := map[string] string {"k2":"v2", "k1":"v1"}
+	as.NotEqual("(%v, %v) should be equivalent", m5, m6)(m5, m6)
+
+	m7 := map[string] string {"k1":"v1", "k3":"v2"}
+	m8 := map[string] string {"k2":"v2", "k1":"v1"}
+	as.NotEqual("(%v, %v) should be equivalent", m7, m8)(m7, m8)
+
+	m9 := map[string] []byte {"k1":s1, "k2":s2}
+	m10 := map[string] []byte {"k1":s1, "k2":s2}
+	as.Equal("(%v, %v) should be equivalent", m9, m10)(m9, m10)
+
+	m11 := map[string] []byte {"k1":s1, "k2":s3}
+	m12 := map[string] []byte {"k1":s1, "k2":s2}
+	as.NotEqual("(%v, %v) should not be equivalent", m11, m12)(m11, m12)
+	
+}
+
+func TestNil(t* testing.T) {
+	as := New(t)
+	if result, _ := AllNil()(nil); !result {
+		as.reportError(1, "AllNil(nil) expects true but got false")
+	}
+	if result, _ := AllNil()(nil, nil); !result {
+		as.reportError(1, "AllNil(nil, nil) expects true but got false")
+	}
+	if result, _ := AllNil()(1); result {
+		as.reportError(1, "AllNil(1) expects false but got true")
+	}
+	if result, _ := AllNil()(1, nil); result {
+		as.reportError(1, "AllNil(1, nil) expects false but got true")
+	}
+	as.IsNil("IsNil(nil) should be true")(nil)
+	as.IsNil("IsNil(nil, nil) should be true")(nil, nil)
+}
