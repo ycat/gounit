4dc070fa08ccc5208b85d2055dd19c93a51bb6f4
ycat <liulitom@gmail.com>
2013-01-08 16:18:42 +0800

    provide seperate method for shallow equal and deep equal.
    shallow equal use == to compare. The uncomparable type (e.g. slice) will panic upon comparing with "==". The panic will be recovered by asserts and report as a test failure.
    deep copy equal reflect.DeepEqual

--- assert/assert.go
+++ assert/assert.go
@@ -5,7 +5,6 @@ import (
 	"runtime"
 	"os"
 	"path/filepath"
-	"errors"
 	"fmt"
 	"reflect"
 )
@@ -40,127 +39,50 @@ func (as *Assert) reportError(cd int, format string, a ...interface{}) {
 
 
 type varArgsFunc func (...interface{})
-type Predicate func (...interface{}) (bool, error)
+type Predicate func (...interface{}) bool
 
 func AllNil() Predicate {
-	return func(ops ...interface{}) (result bool, err error) {
-		result = true
+	return func(ops ...interface{}) bool {
 		for index := 0; index < len(ops); index ++ {
 			if ops[index] != nil {
-				result = false
-				return
+				return false
 			}
 		}
-		return 
+		return true
 	}
 }
 
 func Not(pred Predicate) Predicate {
-	return func(ops ...interface{}) (bool, error) {
-		result, err := pred(ops...)
-		return !result, err
+	return func(ops ...interface{}) bool {
+		return !pred(ops...)
 	}
 }
 
 
-func equalSlice(op1 interface{}, op2 interface{}) bool {
-	s1 := reflect.ValueOf(op1)
-	s2 := reflect.ValueOf(op2)
-	if s1.Len() == s2.Len() {
-		for index := 0; index < s1.Len(); index ++ {
-			if !equal(s1.Index(index).Interface(), s2.Index(index).Interface()) {
-				return false
-			}
-		}
-		return true
-	} 
-	return false
-}
-
-func equalMap(op1 interface{}, op2 interface{}) bool {
-	m1 := reflect.ValueOf(op1)
-	m2 := reflect.ValueOf(op2)
-	if m1.Len() == m2.Len() {
-		keys := m1.MapKeys()
-		for _, key := range keys {
-			value1 := m1.MapIndex(key)
-			value2 := m2.MapIndex(key)
-			if !value2.IsValid() {
-				return false
-			}
-			if !equal(value1.Interface(), value2.Interface()) {
-				return false
-			}
-		}
-		return true
-	}
-	return false
-}
-
-func alwaysNotEqual(op1 interface{}, op2 interface{}) bool {
-	return false
-}
-
-func selectEqualFunction(op1 interface{}, op2 interface{}) (func (interface{}, interface{}) bool) {
-	kind1 := reflect.TypeOf(op1).Kind()
-	kind2 := reflect.TypeOf(op2).Kind()
-	
-	isSlice1 := kind1 == reflect.Slice || kind1 == reflect.Array
-	isSlice2 := kind2 == reflect.Slice || kind2 == reflect.Array
-
-	if isSlice1 != isSlice2 {
-		return alwaysNotEqual
-	}
-	
-	if isSlice1 && isSlice2 {
-		return equalSlice
-	}
-
-	isMap1 := kind1 == reflect.Map
-	isMap2 := kind2 == reflect.Map
-	if isMap1 != isMap2 {
-		return alwaysNotEqual
-	}
-	
-	if isMap1 && isMap2 {
-		return equalMap
-	}
-	
-	return equalDefault
-}
-
-func equalDefault(op1 interface{}, op2 interface{}) bool {
-	return op1 == op2
-}
-
-func equal(op1 interface{}, op2 interface{}) bool {
-	return selectEqualFunction(op1, op2)(op1, op2)
-}
-
-func Eq() Predicate {
-	return func(ops ...interface{}) (result bool, err error) {
+func Eq(equalFunc (func (interface{}, interface{}) bool)) Predicate {
+	return func(ops ...interface{}) bool {
 		if len(ops) <= 1 {
-			err = errors.New(fmt.Sprintf("too few arguments for Equivalance: %v", ops))
-			return
+			panic(fmt.Sprintf("too few arguments for Equivalance: %v", ops))
 		}
 		op0 := ops[0]
-		result = true
 		for index := 1; index < len(ops); index ++ {
-			if !equal(op0, ops[index]) {
-				result = false
-				return
+			if !equalFunc(op0, ops[index]) {
+				return false
 			}
 		}
-		return
+		return true
 	}
 }
+
 
 func (as *Assert) Assert(pred Predicate, format string, a ...interface{}) varArgsFunc {
 	return func(ops ...interface{}) {
-		result, err := pred(ops...)
-		if err != nil {
-			as.reportError(2, "%v", err)
-		} else if !result {
+		defer func() {
+			if r := recover(); r != nil {
+				as.reportError(10, "panic with following parameters %v. Panic: %v", a, r)
+			}
+		}()
+		if !pred(ops...) {
 			as.reportError(2, format, a...)
 		}
 	}
@@ -174,10 +96,22 @@ func (as *Assert) ExistNotNil(format string, a ...interface{}) varArgsFunc  {
 	return as.Assert(Not(AllNil()), format, a...)
 }
 
+func shallowEqual(a interface{}, b interface{}) bool {
+	return a == b
+}
+
 func (as *Assert) Equal(format string, a ...interface{}) varArgsFunc {
-	return as.Assert(Eq(), format, a...)
+	return as.Assert(Eq(shallowEqual), format, a...)
 }
 
 func (as *Assert) NotEqual(format string, a ...interface{}) varArgsFunc {
-	return as.Assert(Not(Eq()), format, a...)
+	return as.Assert(Not(Eq(shallowEqual)), format, a...)
+}
+
+func (as *Assert) DeepEqual(format string, a ...interface{}) varArgsFunc {
+	return as.Assert(Eq(reflect.DeepEqual), format, a...)
+}
+
+func (as *Assert) NotDeepEqual(format string, a ...interface{}) varArgsFunc {
+	return as.Assert(Not(Eq(reflect.DeepEqual)), format, a...)
 }
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -2,20 +2,14 @@ package assert
 
 import "testing"
 
-func TestEq(t* testing.T) {
+type TestStruct struct {
+	name   string
+	amount int
+	data   []byte
+}
+
+func TestEq(t *testing.T) {
 	as := New(t)
-	if result, _ := Eq()(1, 1); !result {
-		as.reportError(1, "Eq()(1, 1) expects true but got false")
-	}
-	if result, _ := Eq()(1, 1, 1); !result {
-		as.reportError(1, "Eq()(1, 1, 1) expects true but got false")
-	}
-	if result, _ := Eq()(1, 2); result {
-		as.reportError(1, "Eq()(1, 2) expects false but got true")
-	}
-	if result, _ := Eq()(1, 2, 1); result {
-		as.reportError(1, "Eq()(1, 2, 1) expects false but got true")
-	}
 	as.Equal("(%v, %v) should be equivalent", 1, 1)(1, 1)
 	as.Equal("(%v, %v, %v) should be equivalent", 1, 1, 1)(1, 1, 1)
 	as.NotEqual("(%v, %v) should not be equivalent", 1, 2)(1, 2)
@@ -31,72 +25,69 @@ func TestEq(t* testing.T) {
 
 	s1 := a1[1:]
 	s2 := a2[1:]
-	as.Equal("(%v, %v) should be equivalent", s1, s2)(s1, s2)
+	as.DeepEqual("(%v, %v) should be equivalent", s1, s2)(s1, s2)
 
 	s3 := a3[:]
 	s4 := a4[:]
-	as.NotEqual("(%v, %v) should not be equivalent", s3, s4)(s3, s4)
+	as.NotDeepEqual("(%v, %v) should not be equivalent", s3, s4)(s3, s4)
 
 	a5 := [3][3]byte{ {1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	a6 := [3][3]byte{ {1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	as.Equal("(%v, %v) should be equivalent", a5, a6)(a5, a6)
+	as.DeepEqual("(%v, %v) should be equivalent", a5, a6)(a5, a6)
 
 	s5 := a5[:]
 	s6 := a6[:]
-	as.Equal("(%v, %v) should be equivalent", s5, s6)(s5, s6)
+	as.DeepEqual("(%v, %v) should be equivalent", s5, s6)(s5, s6)
 
 	s7 := a5[0:2]
 	s8 := a6[1:3]
-	as.NotEqual("(%v, %v) should not be equivalent", s7, s8)(s7, s8)
-	
+	as.NotDeepEqual("(%v, %v) should not be equivalent", s7, s8)(s7, s8)
+
 	a7 := [2][]byte{s1, s2}
 	a8 := [2][]byte{s1, s2}
-	as.Equal("(%v, %v) should be equivalent", a7, a8)(a7, a8)
-	
+	as.DeepEqual("(%v, %v) should be equivalent", a7, a8)(a7, a8)
+
 	s9 := a7[:]
 	s10 := a8[:]
-	as.Equal("(%v, %v) should be equivalent", s9, s10)(s9, s10)
+	as.DeepEqual("(%v, %v) should be equivalent", s9, s10)(s9, s10)
 
-	m1 := make(map[string] int)
-	m2 := make(map[string] int)
-	as.Equal("(%v, %v) should be equivalent", m1, m2)(m1, m2)
-	
-	m3 := map[string] string {"k1":"v1", "k2":"v2"}
-	m4 := map[string] string {"k2":"v2", "k1":"v1"}
-	as.Equal("(%v, %v) should be equivalent", m3, m4)(m3, m4)
+	m1 := make(map[string]int)
+	m2 := make(map[string]int)
+	as.DeepEqual("(%v, %v) should be equivalent", m1, m2)(m1, m2)
 
-	m5 := map[string] string {"k1":"v1", "k2":"v3"}
-	m6 := map[string] string {"k2":"v2", "k1":"v1"}
-	as.NotEqual("(%v, %v) should be equivalent", m5, m6)(m5, m6)
+	m3 := map[string]string{"k1": "v1", "k2": "v2"}
+	m4 := map[string]string{"k2": "v2", "k1": "v1"}
+	as.DeepEqual("(%v, %v) should be equivalent", m3, m4)(m3, m4)
 
-	m7 := map[string] string {"k1":"v1", "k3":"v2"}
-	m8 := map[string] string {"k2":"v2", "k1":"v1"}
-	as.NotEqual("(%v, %v) should be equivalent", m7, m8)(m7, m8)
+	m5 := map[string]string{"k1": "v1", "k2": "v3"}
+	m6 := map[string]string{"k2": "v2", "k1": "v1"}
+	as.NotDeepEqual("(%v, %v) should be equivalent", m5, m6)(m5, m6)
 
-	m9 := map[string] []byte {"k1":s1, "k2":s2}
-	m10 := map[string] []byte {"k1":s1, "k2":s2}
-	as.Equal("(%v, %v) should be equivalent", m9, m10)(m9, m10)
+	m7 := map[string]string{"k1": "v1", "k3": "v2"}
+	m8 := map[string]string{"k2": "v2", "k1": "v1"}
+	as.NotDeepEqual("(%v, %v) should be equivalent", m7, m8)(m7, m8)
 
-	m11 := map[string] []byte {"k1":s1, "k2":s3}
-	m12 := map[string] []byte {"k1":s1, "k2":s2}
-	as.NotEqual("(%v, %v) should not be equivalent", m11, m12)(m11, m12)
-	
+	m9 := map[string][]byte{"k1": s1, "k2": s2}
+	m10 := map[string][]byte{"k1": s1, "k2": s2}
+	as.DeepEqual("(%v, %v) should be equivalent", m9, m10)(m9, m10)
+
+	m11 := map[string][]byte{"k1": s1, "k2": s3}
+	m12 := map[string][]byte{"k1": s1, "k2": s2}
+	as.NotDeepEqual("(%v, %v) should not be equivalent", m11, m12)(m11, m12)
+
+	t1 := TestStruct{"Tom", 1, s1}
+	t2 := TestStruct{"Tom", 1, s2}
+	as.DeepEqual("(%v, %v) should be equivalent", t1, t2)(t1, t2)
+
+	t3 := TestStruct{"Tom", 1, s1}
+	t4 := TestStruct{"Anna", 2, s1}
+	as.NotDeepEqual("(%v, %v) should not be equivalent", t3, t4)(t3, t4)
+
 }
 
-func TestNil(t* testing.T) {
+func TestNil(t *testing.T) {
 	as := New(t)
-	if result, _ := AllNil()(nil); !result {
-		as.reportError(1, "AllNil(nil) expects true but got false")
-	}
-	if result, _ := AllNil()(nil, nil); !result {
-		as.reportError(1, "AllNil(nil, nil) expects true but got false")
-	}
-	if result, _ := AllNil()(1); result {
-		as.reportError(1, "AllNil(1) expects false but got true")
-	}
-	if result, _ := AllNil()(1, nil); result {
-		as.reportError(1, "AllNil(1, nil) expects false but got true")
-	}
 	as.IsAllNil("IsNil(nil) should be true")(nil)
 	as.IsAllNil("IsNil(nil, nil) should be true")(nil, nil)
+	as.ExistNotNil("ExistNotNil(nil, 1) should be true")(nil, 1)
 }
